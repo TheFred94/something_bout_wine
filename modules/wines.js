07c9c9c59fe1cec3b6d1a6462a6b10e3852a1a82
TheFred94 <frederikrlarsen@gmail.com>
2023-04-28 08:54:01 +0200

    Comments on the code

--- modules/wines.js
+++ modules/wines.js
@@ -1,29 +1,34 @@
+// Import the API_KEY and ENDPOINT from a config file
 import { API_KEY, ENDPOINT } from "./config.js";
+
+// GET function that fetches data from the API endpoint and renders it on the page
 export function get() {
   fetch(ENDPOINT, {
     method: "get",
     headers: {
-      apikey: API_KEY,
+      apikey: API_KEY, // Set the API key in the headers
     },
   })
-    .then((e) => e.json())
+    .then((e) => e.json()) // Parse the response as JSON
     .then((data) => {
-      const t = document.querySelector("template").content;
-      document.querySelector("main").innerHTML = "";
+      const t = document.querySelector("template").content; // Get the content of the template tag
+      document.querySelector("main").innerHTML = ""; // Clear the main element's HTML
       data.forEach((wine) => {
-        const copy = t.cloneNode(true);
-        copy.querySelector("h2").textContent = wine.name;
+        // Loop through the data
+        const copy = t.cloneNode(true); // Clone the template
+        copy.querySelector("h2").textContent = wine.name; // Set the wine name
         copy.querySelector("button.delete").addEventListener("click", () => {
-          deleteWine(wine.id);
+          deleteWine(wine.id); // Add event listener to delete the wine
         });
         copy.querySelector("button.update").addEventListener("click", () => {
-          patch(wine.id);
+          patch(wine.id); // Add event listener to update the wine
         });
-        document.querySelector("main").appendChild(copy);
+        document.querySelector("main").appendChild(copy); // Append the wine to the main element
       });
     });
 }
 
+// POST function that creates a new wine and sends it to the API endpoint
 export function post() {
   const newWine = {
     name: "Peter's Wine",
@@ -42,28 +47,29 @@ export function post() {
     headers: {
       "Content-Type": "application/json",
       Prefer: "return=representation",
-      apikey: API_KEY,
+      apikey: API_KEY, // Set the API key in the headers
     },
-    body: JSON.stringify(newWine),
+    body: JSON.stringify(newWine), // Set the request body as the new wine object in JSON format
   })
-    .then((e) => e.json())
-    .then((e) => get());
+    .then((e) => e.json()) // Parse the response as JSON
+    .then((e) => get()); // Fetch the updated data from the API and render it on the page
 }
 
-//DELETE
+// DELETE function that deletes a wine from the API endpoint by ID
 export function deleteWine(id) {
   fetch(ENDPOINT + "?id=eq." + id, {
     method: "delete",
     headers: {
       "Content-Type": "application/json",
       Prefer: "return=representation",
-      apikey: API_KEY,
+      apikey: API_KEY, // Set the API key in the headers
     },
   })
-    .then((e) => e.json())
-    .then((e) => get());
+    .then((e) => e.json()) // Parse the response as JSON
+    .then((e) => get()); // Fetch the updated data from the API and render it on the page
 }
 
+// PATCH function that updates a wine from the API endpoint by ID
 export function patch(id) {
   const updates = {
     name: "Ullas Wine",
@@ -75,10 +81,8 @@ export function patch(id) {
     headers: {
       "Content-Type": "application/json",
       Prefer: "return=representation",
-      apikey: API_KEY,
+      apikey: API_KEY, // Set the API key in the headers
     },
-    body: JSON.stringify(updates),
-  })
-    .then((e) => e.json())
-    .then((e) => get());
+    body: JSON.stringify(updates), // Set the request body as the updated wine object in JSON format
+  }).then((e) => e.json()); // Parse the response as JSON
 }
